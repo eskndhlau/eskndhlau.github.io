988485c6f59691114d0798267aa0ef284ca8a05f
eskndhlau <121695186+eskndhlau@users.noreply.github.com>
2023-04-22 05:37:56 -0500

    Adding Favicon

---
+++ js/offcanvas.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidemenu").style.width = "80vw";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidemenu").style.width = "0";
+  document.body.style.backgroundColor = "white";
+} 
